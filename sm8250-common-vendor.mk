1ad73828c8c5bffcdad62bd9c01f139e50926bdc
bxySo <bxyso@outlook.com>
2025-03-30 15:56:14 +0800

    Remove duplicate proprietary libraries

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -207,9 +207,6 @@ PRODUCT_PACKAGES += \
     libaudcal \
     libbluetooth_audio_session_qti \
     libbluetooth_audio_session_qti_2_1 \
-    libcapiv2svacnn \
-    libcapiv2svarnn \
-    libcapiv2vop \
     libdeccfg \
     libfastcrc \
     libgcs-calwrapper \
